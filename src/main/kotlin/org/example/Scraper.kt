930b87c0ce86054bf61b3519f44f36e6a2235702
p-adams <ghusiyy@gmail.com>
2021-07-03 11:03:15 -0700

    create extract biography data class and add gson dependency

--- src/main/kotlin/org/example/Scraper.kt
+++ src/main/kotlin/org/example/Scraper.kt
@@ -1,6 +1,13 @@
 package org.example
+import com.google.gson.Gson
 import org.jsoup.Jsoup;
+
+
+data class ExtractedBiography(val id: String, val data: String)
+
 
 fun scrape() {
     val doc = Jsoup.connect(System.getenv("url")).get()
+    val gson = Gson()
+    println(gson.toJson(ExtractedBiography("1", "أحمد ابن إبراهيم ابن خالد الموصلي أبو علي نزيل بغداد صدوق من العاشرة مات سنة ست وثلاثين د فق")))
 }
