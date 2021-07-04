cd6c36583d4e5fb2ee3feac176725a3d7bee7f53
p-adams <ghusiyy@gmail.com>
2021-07-04 15:11:38 -0700

    iterate over first items to extract index key

--- src/main/kotlin/org/example/Scraper.kt
+++ src/main/kotlin/org/example/Scraper.kt
@@ -1,9 +1,10 @@
 package org.example
 import com.google.gson.Gson
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-val doc = Jsoup.connect(System.getenv("url")).get()
+val doc: Document = Jsoup.connect(System.getenv("url")).get()
 val gson = Gson()
 
 data class ExtractedBiography(val id: String, val data: String)
@@ -14,5 +15,10 @@ fun liToJson(liElement: Element): String? {
 
 fun scrape() {
     val indexList = doc.select(".betaka-index > ul")
-    println(indexList)
+    for(ul in indexList.iterator()) {
+        val firstIndex = ul.selectFirst("li")
+        val index = firstIndex.select("li > a").textNodes().first()
+        // create new JSON entry keyed by index
+        println(index)
+    }
 }
