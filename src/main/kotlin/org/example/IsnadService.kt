7d61aba9230642172c587275c1538042674217a5
p-adams <ghusiyy@gmail.com>
2021-07-15 00:17:39 -0700

    blueprint for tokenize function

--- src/main/kotlin/org/example/IsnadService.kt
+++ src/main/kotlin/org/example/IsnadService.kt
@@ -1,4 +1,10 @@
 package org.example
 
 class IsnadService {
+    // tokenize takes raw isnad string input and return list of tokens
+    fun tokenize(isnadString: String): List<String> {
+        return listOf()
+    }
 }
+
+
