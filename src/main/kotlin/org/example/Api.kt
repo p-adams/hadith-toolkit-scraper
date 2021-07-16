af03e0749adba2d60523a087340cbe0aa7854795
p-adams <ghusiyy@gmail.com>
2021-07-16 15:54:56 -0700

    init launchApi function

--- src/main/kotlin/org/example/Api.kt
+++ src/main/kotlin/org/example/Api.kt
@@ -1,2 +1,14 @@
 package org.example
 
+import io.javalin.Javalin
+
+fun launchApi() {
+    Javalin.create { config ->
+        config.requestLogger { ctx, ms ->
+            println(ctx)
+            println(ms)
+        }
+    }
+    val app = Javalin.create().start(4000)
+    app.get("/") {ctx -> ctx.result("Test")}
+}
