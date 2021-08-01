d1b61d9e93acfb71a7cd24f564390eda760d2fd2
Paul Adams <ghusiyy@gmail.com>
2021-07-31 22:50:18 -0700

    Use Gson as Json Mapper

--- src/main/kotlin/org/example/Api.kt
+++ src/main/kotlin/org/example/Api.kt
@@ -1,9 +1,24 @@
 package org.example
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import io.javalin.Javalin
+import io.javalin.plugin.json.FromJsonMapper
+import io.javalin.plugin.json.JavalinJson
+import io.javalin.plugin.json.ToJsonMapper
 import java.io.File
 
 fun launchApi() {
+
+    val gson = GsonBuilder().create()
+
+    JavalinJson.fromJsonMapper = object : FromJsonMapper {
+        override fun <T> map(json: String, targetClass: Class<T>) = gson.fromJson(json, targetClass)
+    }
+
+    JavalinJson.toJsonMapper = object : ToJsonMapper {
+        override fun map(obj: Any): String = gson.toJson(obj)
+    }
+
     Javalin.create { config ->
         config.requestLogger { ctx, ms ->
             println(ctx)
@@ -13,6 +28,6 @@ fun launchApi() {
     val app = Javalin.create().start(4001)
     app.get("/taqrib_raw") { ctx ->
         val text = File("src/main/kotlin/org/example/assets/taqrib_raw.json").readText()
-        ctx.result(text)
+        ctx.json(text)
     }
 }
