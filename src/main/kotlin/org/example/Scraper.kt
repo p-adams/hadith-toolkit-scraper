8ea716da47ef202d16a17d0e7414cf489f0bc497
p-adams <ghusiyy@gmail.com>
2021-08-18 14:31:40 -0700

    scraper rewrite to address incorrectly extracted entries

--- src/main/kotlin/org/example/Scraper.kt
+++ src/main/kotlin/org/example/Scraper.kt
@@ -13,35 +13,50 @@ val gson = Gson()
 
 data class ExtractedBiography(val id: String, val data: String)
 
-fun scrape() {
-    var extractedBiographies = mutableListOf<ExtractedBiography>()
+
+fun pageRanges(): MutableList<Int> {
+    val rangeList = mutableListOf<Int>()
     val indexList = doc.select(".betaka-index > ul")
     for(ul in indexList.iterator()) {
         val listItems = ul.select("li")
-        for(index in  0 until listItems.size - 1) {
+        for (index in 0 until listItems.size - 1) {
             val link = listItems[index].select("a")
             val nextLink = listItems[index + 1].select("a")
             val href = link.attr("href")
             val nextHref = nextLink.attr("href")
-            if(href != "javascript:;" && nextHref != "javascript:;") {
-                val path = Paths.get(href).fileName.toString().toInt()
-                val nextPath = Paths.get(nextHref).fileName.toString().toInt()
-                val range = path..nextPath
-                if(path != nextPath) {
-                    for(page in range) {
-                        val currentPage = Jsoup.connect("$BASE_URL/$page").get().select(".nass > p")
-                        val currentPageTextNodes = currentPage.textNodes()
-                        for(node in currentPageTextNodes) {
-                            // add each text node to list
-                            val text = node.text()
-                            val splitText = text.split("-")
-                            val biographyIndex = splitText.first()
-                            val biographyText = splitText.last()
-                            extractedBiographies.add(ExtractedBiography(biographyIndex, biographyText))
-                        }
-                    }
-                }
+            if (href == "javascript:;") {
+                rangeList.add(Paths.get(nextHref).fileName.toString().toInt())
             }
+        }
+    }
+    return rangeList
+}
+
+fun scrape() {
+    var extractedBiographies = mutableListOf<ExtractedBiography>()
+    for(index in 0 until pageRanges().size - 1) {
+        val start = pageRanges()[index]
+        val end = pageRanges()[index + 1]
+        for(page in start until end) {
+            val currentPage = Jsoup.connect("$BASE_URL/$page").get().select(".nass")
+            val currentPageTextNodes = currentPage.select("p")
+               for(p in currentPageTextNodes) {
+                   // best case scenario is that the text has ID and biographical text data
+                   if(p.text().first().digitToIntOrNull() !== null) {
+                       val splitText = p.text().split("-")
+                       val biographyIndex = splitText.first()
+                       val biographyText = splitText.last()
+                       extractedBiographies.add(ExtractedBiography(biographyIndex , biographyText))
+                   } else {
+                       // handle case where biography entry does not have an ID, in which case
+                       // set ID as "*"
+                       val biographyText = p.text().split("[]").last()
+                       // chapter and section headings sometimes appear here, so only add the biographical data
+                       if(!biographyText.contains("[")) {
+                           extractedBiographies.add(ExtractedBiography("*", biographyText))
+                       }
+                   }
+               }
         }
     }
     val jsonExtractedBiographyList = GsonBuilder()
